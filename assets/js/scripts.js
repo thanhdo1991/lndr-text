8d357b5198a5d54a3f213ea717b003e515197284
Igor <igorputina@gmail.com>
2017-06-29 09:16:47 +0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+// JavaScript
+
+// If there's a `data-background-image` attribute, replace the block
+// background image with the value, inside `url([path])`
+(function ( $ ) {
+
+  var setCustomBgImg = function(el) {
+    el = $(el);
+    var imgUrl = el.attr('data-background-image');
+    imgUrl != "" && (imgUrl = "url(" + imgUrl + ")");
+    el.css('background-image', imgUrl);
+  };
+
+  $("[data-background-image]").each(function(k, el) {
+    setCustomBgImg(el);
+
+    $(el).change(function() {
+      setCustomBgImg(el);
+    });
+  });
+
+}( jQuery ));
